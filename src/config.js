0e1cdc680abc8704571fd36a9c1b5804027fdff1
dima <dima@example.com>
2019-06-15 13:27:01 +0300

    Add mongo config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   TOKEN: '679623199:AAHNyEw8aJ-piLrugYG_JNravwxps0noFsI',
-  DB_URL: 'mongodb://localhost/wfmcinema'
+  DB_URL: 'mongodb+srv://mozgmikky:Playstation4@cluster0-ygynq.gcp.mongodb.net/test?retryWrites=true&w=majority'
 }
